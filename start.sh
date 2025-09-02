584128c7a5aa25d9786959d816a70d05eccb5cf0
Jo_Richard <richardjava9110@gmail.com>
2025-09-02 15:55:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,10 +7,8 @@ until pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER"; do
 done
 echo "Database is ready!"
 
-# Activate bench environment
 cd /home/frappe/frappe-bench
 
-# Create site if it doesn't already exist
 if [ ! -d "/home/frappe/frappe-bench/sites/${SITE_NAME}" ]; then
   echo "Creating new site: ${SITE_NAME}"
   bench new-site ${SITE_NAME} \
@@ -21,13 +19,11 @@ if [ ! -d "/home/frappe/frappe-bench/sites/${SITE_NAME}" ]; then
     --db-user ${DB_USER} \
     --db-password ${DB_PASSWORD} \
     --admin-password Admin12345 \
-    --db-root-password ${DB_PASSWORD} \
-    --install-app erpnext \
-    --no-input
+    --db-root-password ${DB_PASSWORD}
 
-  echo "Site ${SITE_NAME} created and ERPNext installed!"
+  echo "Installing ERPNext app..."
+  bench --site ${SITE_NAME} install-app erpnext
 fi
 
-# Start production WSGI server
 echo "Starting production server on port 8000..."
 exec bench serve --port 8000
