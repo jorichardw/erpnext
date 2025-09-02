27aa2b78912660dc21bede0875d3304fb28c9c59
Jo_Richard <richardjava9110@gmail.com>
2025-09-02 15:47:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -16,10 +16,10 @@ if [ ! -d "/home/frappe/frappe-bench/sites/${SITE_NAME}" ]; then
     --db-user ${DB_USER} \
     --db-password ${DB_PASSWORD} \
     --admin-password Admin12345 \
-    --no-mariadb-socket
+    --root-password ${DB_PASSWORD}
 
   bench --site ${SITE_NAME} install-app erpnext
 fi
 
-# Run the production-style server
+# Start the production server
 bench serve --port 8000
