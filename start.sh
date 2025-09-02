4cd76dd04100869ba1bdbafd252a79f5c21ea9ff
Jo_Richard <richardjava9110@gmail.com>
2025-09-02 16:02:47 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,7 +10,6 @@ echo "Database is ready!"
 cd /home/frappe/frappe-bench
 
 if [ ! -d "/home/frappe/frappe-bench/sites/${SITE_NAME}" ]; then
-  echo "Creating new site: ${SITE_NAME}"
   bench new-site ${SITE_NAME} \
     --db-type postgres \
     --db-host ${DB_HOST} \
@@ -18,15 +17,13 @@ if [ ! -d "/home/frappe/frappe-bench/sites/${SITE_NAME}" ]; then
     --db-name ${DB_NAME} \
     --db-user ${DB_USER} \
     --db-password ${DB_PASSWORD} \
-    --admin-password Admin12345
+    --admin-password Admin12345 \
+    --db-root-password ${DB_ROOT_PASSWORD}
 
-  # Add sslmode=require to site config
   jq '. + {"db_sslmode":"require"}' sites/${SITE_NAME}/site_config.json > sites/${SITE_NAME}/site_config_tmp.json
   mv sites/${SITE_NAME}/site_config_tmp.json sites/${SITE_NAME}/site_config.json
 
-  echo "Installing ERPNext app..."
   bench --site ${SITE_NAME} install-app erpnext
 fi
 
-echo "Starting production server on port 8000..."
-exec bench serve --port 8000
+bench serve --port 8000
