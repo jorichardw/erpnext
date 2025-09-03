9d5214480334146b02a31eee03e2d05f787422c2
Jo_Richard <richardjava9110@gmail.com>
2025-09-03 12:03:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,26 +7,19 @@ until pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER"; do
 done
 echo "Database is ready!"
 
-cd /home/frappe/frappe-bench
-
-export PGSSLMODE=require
-
 if [ ! -d "/home/frappe/frappe-bench/sites/${SITE_NAME}" ]; then
-  echo "Creating new site: ${SITE_NAME}"
-  bench new-site "${SITE_NAME}" \
+  bench new-site ${SITE_NAME} \
     --db-type postgres \
-    --db-host "${DB_HOST}" \
-    --db-port "${DB_PORT}" \
-    --db-name "${DB_NAME}" \
-    --db-user "${DB_USER}" \
-    --db-password "${DB_PASSWORD}" \
+    --db-host ${DB_HOST} \
+    --db-port ${DB_PORT} \
+    --db-name ${DB_NAME} \
+    --db-user ${DB_USER} \
+    --db-password ${DB_PASSWORD} \
     --admin-password Admin12345 \
-    --db-root-password "${DB_PASSWORD}"
+    --no-mariadb-check
 
-      jq '. + {"db_sslmode":"require"}' sites/${SITE_NAME}/site_config.json > sites/${SITE_NAME}/site_config_tmp.json
-  mv sites/${SITE_NAME}/site_config_tmp.json sites/${SITE_NAME}/site_config.json
-
-  bench --site "${SITE_NAME}" install-app erpnext
+  bench --site ${SITE_NAME} install-app erpnext
 fi
 
+# Run the production-style server
 bench serve --port 8000
